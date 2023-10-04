4855b6115ed0eddcd75eeb4847e9701bc90c341d
Jammie00James <omoeiyekewenjames@gmail.com>
2023-10-04 14:48:08 +0100

    Adjustment

--- backend/index.js
+++ backend/index.js
@@ -22,14 +22,20 @@ async function commence() {
     await startApp();
     // await connectMongo();
 
-    const app = express()
-    const corsOptions = {
-      origin: '*', // Replace with your front-end's URL
-    };
+    // const app = express()
+    // const corsOptions = {
+    //   origin: '*', // Replace with your front-end's URL
+    // };
 
-    app.use(cors(corsOptions));
+    // app.use(cors(corsOptions));
 
-
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+      // Add other CORS headers as needed
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      next();
+    });
 
     app.use(bodyParser.json());
     app.use(cookieParser());
